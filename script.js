36147464cc6103620e02db4228ae73925f1c0e2f
Andrew Smith <meuzishun@gmail.com>
2021-07-10 15:57:49 -0400

    Add Sass file

--- script.js
+++ script.js
@@ -3,7 +3,28 @@ const numberBtns = [...document.querySelectorAll('.number-btn')];
 const operatorBtns = [...document.querySelectorAll('.operator-btn')];
 const clearBtn = document.querySelector('.clear-btn');
 
+let displayValue = '';
 
+function updateDisplay() {
+    display.textContent = displayValue;
+}
+
+function clearDisplay() {
+    displayValue = '';
+    updateDisplay();
+}
+
+function appendDisplay(val) {
+    displayValue += val;
+    updateDisplay();
+}
+
+function handleNumberBtn(evt) {
+    const val = evt.target.innerText;
+    appendDisplay(val);
+}
+
+numberBtns.forEach(btn => btn.addEventListener('click', handleNumberBtn));
 
 function add(num1, num2) {
     return num1 + num2;
