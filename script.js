cc0b5b3d9a0578123429725151dd4d87b728c11e
Andrew Smith <meuzishun@gmail.com>
2021-07-11 06:41:49 -0400

    Add functionality for basic math

--- script.js
+++ script.js
@@ -4,19 +4,18 @@ const operatorBtns = [...document.querySelectorAll('.operator-btn')];
 const clearBtn = document.querySelector('.clear-btn');
 
 let displayValue = '';
+let operand1 = null;
+let operand2 = null;
+let operation = null;
 
 function updateDisplay() {
     display.textContent = displayValue;
 }
 
-function clearDisplay() {
-    displayValue = '';
-}
-
 function appendDisplay(val) {
     if (val === '.' && displayValue.includes('.')) return;
     if (val === '0' && displayValue === '0') return;
-    if (val !== '0' && displayValue === '0') clearDisplay();
+    if (val !== '0' && displayValue === '0') displayValue = '';
     displayValue += val;
 }
 
@@ -27,25 +26,41 @@ function handleNumberBtn(evt) {
 }
 
 function handleClearBtn() {
-    clearDisplay();
+    operation = null;
     displayValue = '0';
     updateDisplay();
 }
 
-function add(num1, num2) {
-    return num1 + num2;
+function handleOperatorBtn(evt) {
+    const val = evt.target.textContent;
+    if (val !== '=') {
+        operand1 = +displayValue;
+        operation = operations[val];
+        displayValue = '';
+        return;
+    }
+    if (val === '=') {
+        operand2 = +displayValue;
+        displayValue = operate(operation, operand1, operand2);
+        updateDisplay();
+        displayValue = '';
+        return;
+    }
 }
 
-function subtract(num1, num2) {
-    return num1 - num2;
-}
-
-function multiply(num1, num2) {
-    return num1 * num2;
-}
-
-function divide(num1, num2) {
-    return num1 / num2;
+const operations = {
+    '+': function add(num1, num2) {
+        return num1 + num2;
+    },
+    '−': function subtract(num1, num2) {
+        return num1 - num2;
+    },
+    '×': function multiply(num1, num2) {
+        return num1 * num2;
+    },
+    '÷': function divide(num1, num2) {
+        return num1 / num2;
+    }
 }
 
 function operate(operator, num1, num2) {
@@ -54,3 +69,4 @@ function operate(operator, num1, num2) {
 
 numberBtns.forEach(btn => btn.addEventListener('click', handleNumberBtn));
 clearBtn.addEventListener('click', handleClearBtn);
+operatorBtns.forEach(btn => btn.addEventListener('click', handleOperatorBtn));
