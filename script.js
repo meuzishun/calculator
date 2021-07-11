dd0e8b10feba05db65a9fa953495647213df35b2
Andrew Smith <meuzishun@gmail.com>
2021-07-11 12:14:40 -0400

    Get basic operations working

--- script.js
+++ script.js
@@ -3,47 +3,74 @@ const numberBtns = [...document.querySelectorAll('.number-btn')];
 const operatorBtns = [...document.querySelectorAll('.operator-btn')];
 const clearBtn = document.querySelector('.clear-btn');
 
-let displayValue = '';
+let inputValue = '';
 let operand1 = null;
 let operand2 = null;
 let operation = null;
+let result = null;
+
+function printDetails() {
+    console.table({
+        inputValue,
+        operand1,
+        operand2,
+        operation,
+        result
+    });
+}
 
 function updateDisplay() {
-    display.textContent = displayValue;
+    display.textContent = `${inputValue}`;
+}
+
+function retrieveDisplay() {
+    inputValue = display.textContent;
+    console.log(inputValue);
 }
 
 function appendDisplay(val) {
-    if (val === '.' && displayValue.includes('.')) return;
-    if (val === '0' && displayValue === '0') return;
-    if (val !== '0' && displayValue === '0') displayValue = '';
-    displayValue += val;
+    if (val === '.' && inputValue.includes('.')) return;
+    if (val === '0' && inputValue === '0') return;
+    if (val !== '0' && inputValue === '0') inputValue = '';
+    inputValue += val;
 }
 
 function handleNumberBtn(evt) {
     const val = evt.target.innerText;
     appendDisplay(val);
     updateDisplay();
+    printDetails();
 }
 
 function handleClearBtn() {
+    operand1 = null;
+    operand2 = null;
     operation = null;
-    displayValue = '0';
+    result = null;
+    inputValue = '0';
     updateDisplay();
+    printDetails();
 }
 
 function handleOperatorBtn(evt) {
     const val = evt.target.textContent;
     if (val !== '=') {
-        operand1 = +displayValue;
+        operand1 = result || +inputValue;
         operation = operations[val];
-        displayValue = '';
+        inputValue = '';
+        printDetails();
         return;
     }
     if (val === '=') {
-        operand2 = +displayValue;
-        displayValue = operate(operation, operand1, operand2);
+        operand2 = +inputValue;
+        result = operate(operation, operand1, operand2);
+        inputValue = result;
         updateDisplay();
-        displayValue = '';
+        inputValue = '';
+        printDetails();
+        // operand1 = null;
+        // operand2 = null;
+        // operation = null;
         return;
     }
 }
