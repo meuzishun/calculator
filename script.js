791f863fade9225ebf4eaab8716d6d129b770f29
Andrew Smith <meuzishun@gmail.com>
2021-07-14 19:02:46 -0400

    Iron out most bugs

--- script.js
+++ script.js
@@ -3,7 +3,8 @@ const numberBtns = [...document.querySelectorAll('.number-btn')];
 const operatorBtns = [...document.querySelectorAll('.operator-btn')];
 const clearBtn = document.querySelector('.clear-btn');
 
-let queuedValue = '';
+// Calculator status
+let queuedValue = null;
 let operand1 = null;
 let operand2 = null;
 let operation = null;
@@ -21,45 +22,12 @@ function printDetails() {
     });
 }
 
-function updateDisplay() {
-    display.textContent = queuedValue;
-}
-
-function appendQueuedValue(val) {
-    queuedValue += val;
-}
-
-function getDisplayValue() {
-    return +display.textContent;
-}
-
-function checkQueuedValueIsZero() {
-    return queuedValue === '0';
-}
-
-function checkQueuedValueHasFloat() {
-    return queuedValue.includes('.');
-}
-
-function processDotInput(val) {
-    if (checkQueuedValueHasFloat()) return;
-    appendQueuedValue(val);
-    updateDisplay();
-}
-
-function processZeroInput(val) {
-    if (checkQueuedValueIsZero()) return;
-    appendQueuedValue(val);
-    updateDisplay();
-}
-
-function processNumInput(val) {
-    if (checkQueuedValueIsZero()) {
-        queuedValue = val;
-    } else {
-        appendQueuedValue(val);
-    }
-    updateDisplay();
+function resetStatus() {
+    queuedValue = null;
+    operand1 = null;
+    operand2 = null;
+    operation = null;
+    result = null;
 }
 
 const operations = {
@@ -78,27 +46,218 @@ const operations = {
 }
 
 function operate(operator, num1, num2) {
-    if (operator === '÷' && num2 === '0') return 'error';
-    return operator(num1, num2);
+    const result = `${operator(num1, num2)}`;
+    return validateResult(result);
 }
+
+function validateResult(result) {
+    if (result === 'Infinity') {
+        return 'Error';
+    } else {
+        return result;
+    }
+}
+
+function validateQueuedValue() {
+    if (queuedValue.length > 12) {
+        let dotPosition = queuedValue.indexOf('.');
+        if (dotPosition > -1) {
+            let value = +queuedValue;
+            return value.toFixed(11 - dotPosition);
+        } else {
+            return 'Too large';
+        }
+    } else {
+        return queuedValue;
+    }
+}
+
+function updateDisplay() {
+    queuedValue = validateQueuedValue();
+    display.textContent = `${queuedValue}`;
+    if (queuedValue === 'Too large' || queuedValue === 'Error') {
+        resetStatus();
+        printDetails();
+    }
+}
+
+function getDisplayValue() {
+    return +display.textContent;
+}
+
+function processDotInput(val) {
+    if (queuedValue?.includes('.')) {
+        return;
+    }
+    if (queuedValue === null) queuedValue = '0';
+    queuedValue += val;
+    updateDisplay();
+    printDetails();
+}
+
+function processZeroInput(val) {
+    if (queuedValue === '0' || queuedValue === null) {
+        queuedValue = '0';
+    } else {
+        queuedValue += val;
+    }
+    updateDisplay();
+    printDetails();
+}
+
+function processNumInput(val) {
+    if (queuedValue === '0' || queuedValue === null) {
+        queuedValue = val;
+    } else {
+        queuedValue += val;
+    }
+    updateDisplay();
+    printDetails();
+}
+
+function validateOperation() {}
 
 function handleNumberBtn(evt) {
     const val = evt.target.innerText;
 
-    if (operation && operand1 && operand2) {
-        queuedValue = '0';
-        updateDisplay();
-        operand1 = null;
-        operand2 = null;
-        operation = null;
-        result = null;
-    }
+    // if (queuedValue === null &&
+    //     display.textContent === null &&
+    //     operand1 === null &&
+    //     operation === null &&
+    //     operand2 === null) {
+    //         // take the input and update display
+    //         if (val === '.') {
+    //             processDotInput(val);
+    //             return;
+    //         }
+    //         if (val === '0') {
+    //             processZeroInput(val);
+    //             return;
+    //         }
+    //         processNumInput(val);
+    //         printDetails();
+    //         return;
+    // }
 
-    if (val === '.') processDotInput(val);
-    if (val === '0') processZeroInput(val);
-    if (val !== '.' && val !== '0') processNumInput(val);
-    updateDisplay();
+    // if (queuedValue === null &&
+    //     display.textContent !== null &&
+    //     operand1 === null &&
+    //     operation === null &&
+    //     operand2 === null) {
+    //         // take the input and update display
+    //         if (val === '.') {
+    //             processDotInput(val);
+    //             return;
+    //         }
+    //         if (val === '0') {
+    //             processZeroInput(val);
+    //             return;
+    //         }
+    //         processNumInput(val);
+    //         printDetails();
+    //         return;
+    //     }
+        
+    // if (queuedValue !== null &&
+    //     display.textContent !== null &&
+    //     operand1 === null &&
+    //     operation === null &&
+    //     operand2 === null) {
+    //         // take the input and update display
+    //         if (val === '.') {
+    //             processDotInput(val);
+    //             return;
+    //         }
+    //         if (val === '0') {
+    //             processZeroInput(val);
+    //             return;
+    //         }
+    //         processNumInput(val);
+    //         printDetails();
+    //         return;
+    // }
+        
+    // if (queuedValue !== null &&
+    //     display.textContent !== null &&
+    //     operand1 !== null &&
+    //     operation !== null &&
+    //     operand2 === null) {
+    //         // take the input and update display
+    //         if (val === '.') {
+    //             processDotInput(val);
+    //             return;
+    //         }
+    //         if (val === '0') {
+    //             processZeroInput(val);
+    //             return;
+    //         }
+    //         processNumInput(val);
+    //         printDetails();
+    //         return;
+    // }
+
+    // if (queuedValue === null &&
+    //     display.textContent !== null &&
+    //     operand1 !== null &&
+    //     operation !== null &&
+    //     operand2 === null) {
+    //         // take the input and update display
+    //         if (val === '.') {
+    //             processDotInput(val);
+    //             return;
+    //         }
+    //         if (val === '0') {
+    //             processZeroInput(val);
+    //             return;
+    //         }
+    //         processNumInput(val);
+    //         printDetails();
+    //         return;
+    // }
+        
+    // if (queuedValue === null &&
+    //     display.textContent !== null &&
+    //     operand1 !== null &&
+    //     operation !== null &&
+    //     operand2 !== null) {
+    //         // clear the other data
+    //         resetStatus();
+    //         // take the input and update display
+    //         if (val === '.') {
+    //             processDotInput(val);
+    //             return;
+    //         }
+    //         if (val === '0') {
+    //             processZeroInput(val);
+    //             return;
+    //         }
+    //         processNumInput(val);
+    //         printDetails();
+    //         return;
+    // }
+
+    if (queuedValue === null &&
+        display.textContent !== null &&
+        operand1 !== null &&
+        operation !== null &&
+        operand2 !== null) {
+            // clear the other data
+            resetStatus();
+        }
+            // take the input and update display
+    if (val === '.') {
+        processDotInput(val);
+        return;
+    }
+    if (val === '0') {
+        processZeroInput(val);
+        return;
+    }
+    processNumInput(val);
     printDetails();
+    return;
+
+    
 }
 
 function handleOperatorBtn(evt) {
@@ -108,58 +267,176 @@ function handleOperatorBtn(evt) {
 }
 
 function handleOperator(val) {
-    if (operation && operand1) {
-        operand2 = getDisplayValue();
-        result = operate(operation, operand1, operand2);
-        queuedValue = result;
-        updateDisplay();
-        operand1 = getDisplayValue();
-        operation = operations[val];
-        operand2 = null;
-        queuedValue = '0';
-        printDetails();
-        return;
+    if (queuedValue === null &&
+        display.textContent === null &&
+        operand1 === null &&
+        operation === null &&
+        operand2 === null) {
+            return;
     }
-    operand1 = getDisplayValue();
-    if (result) {
-        queuedValue = '0';
-    } else {
-        queuedValue = '';
+    
+    if (queuedValue !== null &&
+        display.textContent === null &&
+        operand1 === null &&
+        operation === null &&
+        operand2 === null) {
+            return;
     }
-    operand2 = null;
-    operation = operations[val];
-    printDetails();
-    return;
+    
+    if (queuedValue === null &&
+        display.textContent !== null &&
+        operand1 === null &&
+        operation === null &&
+        operand2 === null) {
+            return;
+    }
+
+    if (queuedValue !== null &&
+        display.textContent !== null &&
+        operand1 === null &&
+        operation === null &&
+        operand2 === null) {
+            operand1 = +queuedValue;
+            operation = operations[val];
+            queuedValue = null;
+            printDetails();
+            return;
+    }
+
+    if (queuedValue !== null &&
+        display.textContent !== null &&
+        operand1 !== null &&
+        operation !== null &&
+        operand2 === null) {
+            operand2 = +display.textContent;
+            result = operate(operation, operand1, operand2);
+            queuedValue = result;
+            updateDisplay();
+            queuedValue = null;
+            operand1 = +display.textContent;
+            operand2 = null;
+            operation = operations[val];
+            result = null;
+            printDetails();
+            return;
+    }
+    
+    if (queuedValue === null &&
+        display.textContent !== null &&
+        operand1 !== null &&
+        operation !== null &&
+        operand2 === null) {
+            operation = operations[val];
+            printDetails();
+            return;
+    }
+    
+    if (queuedValue === null &&
+        display.textContent !== null &&
+        operand1 !== null &&
+        operation !== null &&
+        operand2 !== null) {
+            operand1 = +display.textContent;
+            operation = operations[val];
+            operand2 = null;
+            printDetails();
+            return;
+    }
 }
 
 function handleEquals() {
-    if (operand2) {
-        operand1 = getDisplayValue();
-    } else {
-        operand2 = getDisplayValue();
+
+    if (queuedValue === null &&
+        display.textContent === null &&
+        operand1 === null &&
+        operation === null &&
+        operand2 === null) {
+            return;
     }
-    result = operate(operation, operand1, operand2);
-    queuedValue = result;
-    updateDisplay();
-    printDetails();
-    return;
+
+    if (queuedValue === null &&
+        display.textContent !== null &&
+        operand1 === null &&
+        operation === null &&
+        operand2 === null) {
+            return;
+    }
+    
+    if (queuedValue !== null &&
+        display.textContent !== null &&
+        operand1 === null &&
+        operation === null &&
+        operand2 === null) {
+            return;
+    }
+    
+    if (queuedValue !== null &&
+        display.textContent !== null &&
+        operand1 !== null &&
+        operation === null &&
+        operand2 === null) {
+            return;
+    }
+
+    if (queuedValue === null &&
+        display.textContent !== null &&
+        operand1 !== null &&
+        operation !== null &&
+        operand2 === null) {
+            return;
+    }
+
+    if (queuedValue !== null &&
+        display.textContent !== null &&
+        operand1 !== null &&
+        operation !== null &&
+        operand2 === null) {
+            operand2 = +display.textContent;
+            result = operate(operation, operand1, operand2);
+            queuedValue = result;
+            updateDisplay();
+            queuedValue = null;
+            printDetails();
+            return;
+    }
+    
+    if (queuedValue === null &&
+        display.textContent !== null &&
+        operand1 !== null &&
+        operation !== null &&
+        operand2 !== null) {
+            operand1 = +display.textContent;
+            result = operate(operation, operand1, operand2);
+            queuedValue = result;
+            updateDisplay();
+            queuedValue = null;
+            printDetails();
+            return;
+    }
 }
 
 function handleClearBtn() {
-    operand1 = null;
-    operand2 = null;
-    operation = null;
-    result = null;
     queuedValue = '0';
     updateDisplay();
+    resetStatus();
     printDetails();
 }
 
 numberBtns.forEach(btn => btn.addEventListener('click', handleNumberBtn));
-clearBtn.addEventListener('click', handleClearBtn);
 operatorBtns.forEach(btn => btn.addEventListener('click', handleOperatorBtn));
+clearBtn.addEventListener('click', handleClearBtn);
 
 
 
 // When you click a number, it shows up in the display
 // When you click an operator, it is stored in the code UNLESS
+
+function testingDec(val) {
+    let dotPosition = val.indexOf('.');
+    let lastPosition = val.length - 1;
+    let numberOfDec = 11 - dotPosition;
+    // console.log(numberOfDec);
+}
+
+testingDec('23.455645678');
+testingDec('24443.455666');
